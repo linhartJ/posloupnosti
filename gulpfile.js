ba2edfd6989d71f827206d25738710940a3f64ea
j.linhart <j.linhart@quadient.com>
2019-11-13 21:32:14 +0100

    included obfuscation for sensitive data into build process

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,7 @@ function typescriptTask() {
         .pipe(gulp.dest('build/temp'));
 }
 
-function joinJS(){
+function joinJS() {
     gulp.src(["build/temp/functions.js", "build/temp/data.js", "build/temp/p1.js"])
         .pipe(concat("index.js"))
         .pipe(uglify())
@@ -46,6 +46,49 @@ function joinJS(){
         .pipe(concat("index.js"))
         .pipe(uglify())
         .pipe(gulp.dest("build/p3"));
+}
+
+function encryptIndexForTaskGroup(pf) {
+    const allContent = fs.readFileSync("build/" + pf + "/index.js", "utf-8");
+    const allMatches = allContent.match(/new Task\("(.*?)","(.*?)"\)/g)
+        .reduce(function (acc, m) {
+            var m2 = m.match(/new Task\("(.*?)","(.*?)"\)/);
+            acc.push(m2[1]);
+            acc.push(m2[2]);
+            return acc
+        }, []);
+    allMatches.push(allContent.match(/p\dFm="(.*?)"/)[1]);
+    const map = {};
+    allMatches.forEach(s => s.split("").forEach(c => map[c] = 0));
+    const array = [];
+    for (var c in map) array.push(c);
+    shuffle(array);
+    let result = allContent;
+    allMatches.forEach(m => {
+        result = result.replace(`"${m}"`, encrypt(m, array))
+    });
+    result = result.replace("var c=[]", `var c=[${array.map(c => '"' + c + '"').join(",")}]`);
+    fs.writeFileSync("build/" + pf + "/index.js", result);
+}
+
+function encryptSensitiveStuff() {
+    encryptIndexForTaskGroup("p1");
+    encryptIndexForTaskGroup("p2");
+    encryptIndexForTaskGroup("p3");
+    return Promise.resolve()
+}
+
+function encrypt(s, a) {
+    const idxs = s.split("").map(c => a.indexOf(c)).join(", ");
+    return `df([${idxs}])`;
+}
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
 }
 
 function copyIndex() {
@@ -69,4 +112,4 @@ function copyResources() {
         .pipe(gulp.dest("build/p3"));
 }
 
-exports.default = gulp.series(cleanTask, initDirs, typescriptTask, joinJS, copyIndex, copyStylesheet, copyResources, cleanTemp);
+exports.default = gulp.series(cleanTask, initDirs, typescriptTask, joinJS, encryptSensitiveStuff, copyIndex, copyStylesheet, copyResources, cleanTemp);
